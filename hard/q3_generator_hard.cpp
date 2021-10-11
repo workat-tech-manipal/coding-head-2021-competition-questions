66e694ec0a12067c3801072cb33efcf93705d4f1
Ayush Batra <75250217+Ayush-Batra@users.noreply.github.com>
2021-10-11 12:30:42 +0530

    Add files via upload

---
+++ hard/q3_generator_hard.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<random>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+	srand(time(0));
+	while(i++< 8) {
+		long long int r = (rand() % 10000) + 1;
+		cout <<r << "\n";
+	}
+	return 0;
+}
