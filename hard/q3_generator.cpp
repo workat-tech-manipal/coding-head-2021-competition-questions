5bb7df1ba6025144840e336e7f744fc4b64e7ec9
Ayush Batra <75250217+Ayush-Batra@users.noreply.github.com>
2021-10-12 09:41:41 +0530

    Update q3_generator.cpp

--- hard/q3_generator.cpp
+++ hard/q3_generator.cpp
@@ -1,19 +1,16 @@
-#include <stdio.h>
-#include <random>
+#include<stdio.h>
+#include<random>
 #include <iostream>
-
-
 using namespace std;
-
-
 int main()
 {
     int i = 0;
-    srand(time(0));
-    while (i++ < 8)
-    {
-        long long int r = (rand() % 10000) + 1;
-        cout << r << "\n";
-    }
-    return 0;
+	srand(time(0));
+	int t = (rand()%100)+1;
+	cout<<t<<"\n";
+	while(i++< t) {
+		long long int r = (rand() % 10000) + 1;
+		cout <<r << "\n";
+	}
+	return 0;
 }
