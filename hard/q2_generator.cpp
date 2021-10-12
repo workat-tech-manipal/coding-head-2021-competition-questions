53d05a3a5a634e8f512372d673079bf662418544
Ayush Batra <75250217+Ayush-Batra@users.noreply.github.com>
2021-10-12 09:39:37 +0530

    Update q2_generator.cpp

--- hard/q2_generator.cpp
+++ hard/q2_generator.cpp
@@ -1,18 +1,16 @@
-#include <stdio.h>
-#include <random>
+#include<stdio.h>
+#include<random>
 #include <iostream>
 using namespace std;
 int main()
 {
-    srand(time(0));
+	srand(time(0));
     int i = 0;
-
-    int T = rand() % 100;
-    cout << T << '\n';
-    while (i++ < T)
-    {
-        long long int r = (rand() % 59999) + 2;
-        cout << r << " ";
-    }
-    return 0;
+	int t = (rand()%100)+1;
+    cout<<t<<"\n";
+	while(i++< t) {
+		long long int r = (rand() % 59999) + 2;
+		cout <<r << "\n";
+	}
+	return 0;
 }
