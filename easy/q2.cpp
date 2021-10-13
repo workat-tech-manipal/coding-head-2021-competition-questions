51e29273609798fad5fc51fdc2ae3cf34936bd07
Ayush Batra <75250217+Ayush-Batra@users.noreply.github.com>
2021-10-13 22:54:36 +0530

    Update q2.cpp

--- easy/q2.cpp
+++ easy/q2.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include <math.h>
-#include <vector>
-
-using namespace std;
-
-bool canPlaceFlowers(std::vector<bool> &bed, int n)
-{
-    for (int i = 0; i < bed.size(); i++)
-    {
-        if (!bed[i] && (i == 0 || !bed[i - 1]) && (i == bed.size() - 1 || !bed[i + 1]))
-        {
-            bed[i] = 1;
-            n--;
+#include<vector>
+class Solution {
+public:
+    bool canPlacecones(std::vector<bool>& bed, int n) {
+        for (int i = 0; i < bed.size(); i++) {
+            if (!bed[i] && (i == 0 || !bed[i - 1]) && (i == bed.size() - 1 || !bed[i + 1])) {
+                bed[i] = 1;
+                n--;
+            }
         }
+        return bool(n <= 0);
     }
-    return n <= 0;
-}
-
-int main()
-{
-
-    vector<bool> v = {1, 0, 0, 0, 0, 0, 0, 0, 1};
-    cout << ((canPlaceFlowers(v, 4)) ? "YES" : "NO") << '\n';
+};
+int main(){
+   int test_case;
+   std::cin>>test_case;
+   while (test_case--)
+   {
+       Solution ob;
+       int arr_size;
+       int ele;
+       std::cin>>arr_size;
+       std::vector<bool> v;
+       for(int i=0;i<arr_size;i++)
+       {
+           std::cin>>ele;
+           v.push_back(ele);
+       }
+       int cones;
+       std::cin>>cones;
+   std::cout << bool(ob.canPlacecones(v, cones));
+   }
+   
+   
 }
